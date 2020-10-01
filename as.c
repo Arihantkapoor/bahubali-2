c1cb1f9b52cb5120379aa89e60e5d5f3055bca59
Arihantkapoor <62657676+Arihantkapoor@users.noreply.github.com>
2020-10-01 11:42:39 +0530

    Update as.c

--- as.c
+++ as.c
@@ -1,9 +1,10 @@
+// Program to print the ascii values
 #include<stdio.h>
 void main()
 {
 int i;
 for(i=0;i<255;i++)
 {
-printf("the asci value for %d is= %c \n",i,i);
+printf("the ascii value for %d is= %c \n",i,i);
 }
 }
